e9789cbbbaf37f188d2eefcf5e935d32249c37ff
carlospotter <43349953+carlospotter@users.noreply.github.com>
2021-10-08 22:39:48 -0300

    minor

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,11 +1,11 @@
-<p class="text-center mt-10" style="font-family: 'Shadows Into Light', cursive;">    
+<p class="text-center text-2xl mt-10" style="font-family: 'Shadows Into Light', cursive;">    
     
     This very simple website is built with Sveltekit and Tailwind CSS for learning (and fun) purposes. You can find the source code <a href="https://github.com/carlospotter/xkcd-sveltekit" target="_blank">here</a>.
 
 </p>
 
 
-<p class="text-center mt-5" style="font-family: 'Shadows Into Light', cursive;">    
+<p class="text-center text-2xl mt-5" style="font-family: 'Shadows Into Light', cursive;">    
     
     You can (and should, the comics are amazing) access the XKCD official website <a href="https://xkcd.com/" target="_blank">here</a>. The comics on this website are extracted from the <a href="https://xkcd.com/json.html" target="_blank">XKCD API</a>.
 
