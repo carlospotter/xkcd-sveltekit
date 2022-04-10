e9bf9067806e1a597c9f59feb35ae85e56ba610b
Carlos Pötter <43349953+carlospotter@users.noreply.github.com>
2022-04-10 08:49:02 -0300

    Deploy on netlify (#3)
    * deploy: add netlify deployment
    
    * fix: npm packages update

---
+++ .netlify/server/entries/endpoints/api/last.js
@@ -0,0 +1,43 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var stdin_exports = {};
+__export(stdin_exports, {
+  get: () => get
+});
+module.exports = __toCommonJS(stdin_exports);
+async function get() {
+  const urlPrefix = `https://xkcd.com/`;
+  const urlSuffix = `info.0.json`;
+  try {
+    const resLastComic = await fetch(urlPrefix + urlSuffix);
+    const jsonLastComic = await resLastComic.json();
+    const lastComic = {
+      title: jsonLastComic.title,
+      num: jsonLastComic.num,
+      img: jsonLastComic.img
+    };
+    return {
+      status: 200,
+      body: lastComic
+    };
+  } catch {
+    return {
+      status: 500
+    };
+  }
+}
